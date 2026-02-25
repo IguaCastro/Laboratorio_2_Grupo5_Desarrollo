94a742ce26d726cfa0c189836ee5a4f07aa5ee76
IguaCastro <igua.castro@gmail.com>
2026-02-25 17:29:02 -0500

    Terminé todas las validaciones para ScoreBoard, se implementó los detalles faltante como lo es, las condiciones con if

--- Laboratorio_2_Grupo5_Desarrollo/ScoreBoard.java
+++ Laboratorio_2_Grupo5_Desarrollo/ScoreBoard.java
@@ -7,21 +7,36 @@ public class ScoreBoard {
         int option = 0;
         do{
             System.out.println("\n===========================================================");
-            System.out.println("====                  MARCADOR DE PUNTOS                 ====");
+            System.out.println("====                  MARCADOR DE PUNTOS               ====");
             System.out.println("===========================================================");
             System.out.println("===                1. Sumar Puntos                      ===");
             System.out.println("===                2. Restar Puntos                     ===");
-            System.out.println("===                3. Ver Marcador                        ===");
+            System.out.println("===                3. Ver Marcador                      ===");
             System.out.println("===                4. Salir                             ===");
             System.out.println("===========================================================\n");
             option=sc.nextInt();
 
             switch (option) {
-                case 1:
-                    
+                case 1: 
+                    point++;
+                    System.out.println("Has sumado 1 punto");
                     break;
-            
+                case 2:
+                    if (point > 0){
+                        point--;
+                        System.out.println("Has restado 1 punto");
+                    }else{
+                        System.out.println("No puedes tener numeros negativos");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Tu puntuación actual es: " + point);
+                    break;
+                case 4:
+                    System.out.println("Saliendo del Menú");
+                    break;
                 default:
+                    System.out.println("Opción incorrecta, vuelve a elegir una de las opciones que se muestra");
                     break;
             }
         }while (option!=4);
