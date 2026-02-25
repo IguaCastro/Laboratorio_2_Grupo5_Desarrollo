e90cef63203407b6d23c0e823c6815ee0d4ac8e3
IguaCastro <igua.castro@gmail.com>
2026-02-25 01:14:16 -0500

    Terminé de validar todo el programa, todo funciona y los while e if, se implementaron correctamente

--- Laboratorio_2_Grupo5_Desarrollo/GuessNumber.java
+++ Laboratorio_2_Grupo5_Desarrollo/GuessNumber.java
@@ -1,6 +1,30 @@
-import java.util.Scanner;
 package Laboratorio_2_Grupo5_Desarrollo;
-
+import java.util.Scanner;
 public class GuessNumber {
-    
+    public static void main (String[] args){
+        Scanner sc = new Scanner (System.in);
+        int secret = (int)(Math.random() * 50) +1;
+        boolean success = false ;
+        int attemps = 0;
+        System.out.println("\n===ADIVINA EL NUMERO ARCADE===");
+        while (!success ){
+            System.out.println("\nIncerte el numero a adivinar");
+            int number = sc.nextInt();
+            attemps++;
+         if (number == secret){
+            System.out.println("ACERTASTE!!!");
+            success = true;
+        }else if (number < secret) {
+            System.out.println("El numero es bajo");
+        }else {
+            System.out.println("El numero es Alto");
+        }
+    }
+        System.out.println("El numero secreto es: " + secret);
+        System.out.println("Lo lograste en " + attemps + " intentos ");
+        
+
+
+    sc.close();
+    }
 }
