1c1d236b27ba990d9609fcf5085f296698f67eaa
yirajordan45 <yirajordan45@gmail.com>
2026-03-06 18:35:27 -0500

    se agrego nuevas variables

--- Laboratorio_2_Grupo5_Desarrollo/GeneratorLevel.java
+++ Laboratorio_2_Grupo5_Desarrollo/GeneratorLevel.java
@@ -1,45 +1,77 @@
 package Laboratorio_2_Grupo5_Desarrollo;
+
 import java.util.Scanner;
+import java.util.Random;
+
 public class GeneratorLevel {
+
     public static void main(String[] args) {
 
-        try (Scanner scanner = new Scanner(System.in)) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
-            System.out.println("Generador de Niveles");
-            System.out.print("Elige un nivel (1-3): ");
-            int level = scanner.nextInt();
+        System.out.println("=== Generador de Niveles ===");
 
-            int height = 0;
-            int width = 0;
+        System.out.print("Elige un nivel (1-3): ");
+        int level = scanner.nextInt();
 
-            switch (level) {
-                case 1:
-                    height = 5;
-                    width = 10;
-                    break;
-                case 2:
-                    height = 7;
-                    width = 15;
-                    break;
-                case 3:
-                    height = 10;
-                    width = 20;
-                    break;
-                default:
-                    System.out.println("Nivel no válido");
-                    return;
-            }
+        System.out.print("Ingrese la altura del mapa: ");
+        int height = scanner.nextInt();
 
-            for (int i = 0; i < height; i++) {
-                for (int j = 0; j < width; j++) {
-                    if (i == 0 || i == height - 1 || j == 0 || j == width - 1) {
-                        System.out.print("*");
-                    } else {
-                        System.out.print(" ");
+        System.out.print("Ingrese el ancho del mapa: ");
+        int width = scanner.nextInt();
+
+        System.out.println("\nMapa generado:\n");
+
+        switch (level) {
+
+            case 1:
+                System.out.println("Nivel 1: Campo de estrellas");
+                for (int i = 0; i < height; i++) {
+                    for (int j = 0; j < width; j++) {
+                        System.out.print("* ");
                     }
+                    System.out.println();
                 }
-                System.out.println();
-            }
+                break;
+
+            case 2:
+                System.out.println("Nivel 2: Bloques aleatorios");
+                for (int i = 0; i < height; i++) {
+                    for (int j = 0; j < width; j++) {
+
+                        if (random.nextInt(2) == 0) {
+                            System.out.print("# ");
+                        } else {
+                            System.out.print(". ");
+                        }
+
+                    }
+                    System.out.println();
+                }
+                break;
+
+            case 3:
+                System.out.println("Nivel 3: Mapa con bordes");
+
+                for (int i = 0; i < height; i++) {
+                    for (int j = 0; j < width; j++) {
+
+                        if (i == 0 || i == height - 1 || j == 0 || j == width - 1) {
+                            System.out.print("# ");
+                        } else {
+                            System.out.print(". ");
+                        }
+
+                    }
+                    System.out.println();
+                }
+                break;
+
+            default:
+                System.out.println("Nivel no válido.");
         }
+
+        scanner.close();
     }
 }
